e9e30ccdb323f50cb5b415fec98a9c33b5bc32b4
Anas AIT ALI <aitali.anas@gmail.com>
2016-10-16 17:53:20 +0100

    Customized theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,70 @@
 /**
- * Main JS file for Casper behaviours
- */
+* Main JS file for Casper behaviours
+*/
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $(document).ready(function () {
+  $(document).ready(function () {
 
-        // Tooltip init
-        tooltipInit();
+    // Tooltip init
+    tooltipInit();
 
-        // Init the posts
-        postInit();
+    // Init the posts
+    postInit();
 
-        // Waypoints
-        waypointsInit();
+    // Waypoints
+    waypointsInit();
 
+  });
+
+  // Init waypoints for header and footer animations
+  function waypointsInit() {
+
+    $('#masthead')
+    .delay(800)
+    .queue( function(next){
+      $(this).addClass('animation-on');
+      next();
     });
 
-    // Init waypoints for header and footer animations
-    function waypointsInit() {
+    var headerWaypoint = new Waypoint({
+      element: document.getElementById('masthead'),
+      handler: function (direction) {
+        if (direction === 'down')
+        this.element.classList.remove('animation-on');
+        else
+        this.element.classList.add('animation-on');
+      },
+      offset: -5
+    });
 
-        var headerWaypoint = new Waypoint({
-            element: document.getElementById('masthead'),
-            handler: function (direction) {
-                if (direction === 'down')
-                    this.element.classList.remove('animation-on');
-                else
-                    this.element.classList.add('animation-on');
-            },
-            offset: -5
-        });
+    var footerWaypoint = new Waypoint({
+      element: document.getElementById('footer'),
+      handler: function (direction) {
+        this.element.classList.toggle('animation-on');
+      },
+      offset: 'bottom-in-view'
+    });
 
-        var footerWaypoint = new Waypoint({
-            element: document.getElementById('footer'),
-            handler: function (direction) {
-                this.element.classList.toggle('animation-on');
-            },
-            offset: 'bottom-in-view'
-        });
+  }
 
-    }
+  // Init bootstrap tooltip
+  function tooltipInit() {
+    $('[data-toggle]').tooltip();
+  }
 
-    // Init bootstrap tooltip
-    function tooltipInit() {
-        $('[data-toggle]').tooltip();
-    }
+  function postInit() {
+    // Set lead paragraphs
+    $('.post-body p:first-child').addClass('lead');
 
-    function postInit() {
-        // Set lead paragraphs
-        $('.post-body p:first-child').addClass('lead');
-
-        // Set feature image
-        var featured = $('.featured-image').find('img').attr('src');
-        if (featured) {
-            $('#masthead').css('backgroundImage', 'url(' + featured + ')');
-            $('#footer').css('backgroundImage', 'url(' + featured + ')');
-        };
-    }
+    // Set feature image
+    var featured = $('.featured-image').find('img').attr('src');
+    if (featured) {
+      $('#masthead').css('backgroundImage', 'url(' + featured + ')');
+      $('#footer').css('backgroundImage', 'url(' + featured + ')');
+    };
+  }
 
 } (jQuery));
-
